0480e5d89cce5fdadd58e12c11330f93311146ee
Elaine Tennant <et1083@users.noreply.github.com>
2016-08-03 13:41:49 -0700

    preliminary code for most popular hashtags
    code does not work.  hashtag is not the right key to use.  entities will give you a list of metadata including hashtags but i have not figured a way to extricate the hashtags yet.

---
+++ top_ten.py
@@ -0,0 +1,39 @@
+import sys
+import json
+from pprint import pprint
+
+def hw():
+    print 'Hello, world!'
+
+def lines(fp):
+    print str(len(fp.readlines()))
+
+def main():
+    tweet_file = open(sys.argv[1])
+    hw()
+    
+    lines(tweet_file)
+     
+    #open output.txt and put it new_tweet_file
+    new_tweet_file = open("output.txt")	
+  
+    #for loop for each line(tweet) in new_tweet_file
+    for line in new_tweet_file:
+        tweet = json.loads(line)   #this makes a json type thing named tweets
+        if 'hashtags' in tweet:	   #looks for the key 'text' in the tweets file 	
+                                 #'text' points toward the text of the tweet          
+            print 'a'
+	    #info = json.loads(tweet['entities'])
+	    #print '# ',  tweet['entities']  #.encode('ascii', 'ignore'))	  
+            #prints mytweet then the text associated with the 'text' key  
+            #.encode makes the unicode work
+
+            #one_tweet holds 1 tweet at a time
+            #one_tweet = tweet['text'].encode('ascii', 'ignore')   
+            
+            #words = one_tweet.split()    #this will break the tweet into words    
+           
+			
+   
+if __name__ == '__main__':
+    main()
